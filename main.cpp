f81cec1f8f5fe78348af820c0f4b956575194bdc
pogorelov <max-pogorelov@mail.ru>
2016-09-27 17:19:49 +0300

    vvod chisel

--- main.cpp
+++ main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
+int a,b;
+cout << "vvod chisel:";
+cin >>a>>b;
+cout <<"a + b = " << a + b << '\n'
+     <<"a + b = " << a - b << '\n'
+     <<"a + b = " << a * b << '\n';
 
 }
